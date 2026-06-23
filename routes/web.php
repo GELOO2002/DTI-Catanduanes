e840f6e0b0bdf081532b42703dc2b61afb65f341
Admin <admin@dti.com>
2026-06-23 11:36:50 +0530

    Add login redirect to admin

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\IslandProfileController;
 
+Route::redirect('/login', '/admin/login');
+
 Route::get('/island-profiles', [IslandProfileController::class, 'index'])->name('island-profiles.index');
 
 Route::get('/', [DashboardController::class, 'index']);
